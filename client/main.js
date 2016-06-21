3f8e6b35f201932bcbcb1eaef2ef598507fb4ee0
Jonathan <jon.wong92@gmail.com>
2016-06-21 15:09:43 -0700

    Refactored to correct client/server files with added security to the game

--- client/main.js
+++ client/main.js
@@ -1,77 +1,67 @@
 PlayersList = new Mongo.Collection('players');
 
-if (Meteor.isClient) {
-  Meteor.subscribe('thePlayers');
-  Template.leaderboard.helpers({
-    'player': function() {
-      return PlayersList.find({}, {sort: {score: -1, name: 1}});
-      // pass arbitrary {} so we can pass another argument e.g. sort
-      // Sort by + => DESC to ASC
-      // Sort by - => ASC to DESC
-    },
-    'listPlayerCount': function() {
-      var currentUserId = Meteor.userId();
-      return PlayersList.find({createdBy: currentUserId}).count();
-    },
-    'selectedClass': function() {
-      var playerId = this._id;
-      var selectedPlayer = Session.get('selectedPlayer');
-      if (playerId == selectedPlayer) {
-        return "selected";
-      }
-    },
-    'showSelectedPlayer': function() {
-      var selectedPlayer = Session.get('selectedPlayer');
-      return PlayersList.findOne(selectedPlayer);
-    },
-    'averageScore': function() {
-      return PlayersList.find({}, {$avg: {score: 1}});
-    }
-  });
-  Template.leaderboard.events({
-    'click .player': function() {
-      var playerId = this._id;
-      Session.set('selectedPlayer', playerId);
-    },
-    'click .increment': function() {
-      var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer, {$inc: {score: 5}});
-    },
-    'click .decrement': function() {
-      var selectedPlayer = Session.get('selectedPlayer');
-      PlayersList.update(selectedPlayer, {$inc: {score: -5}});
-    }
-  });
+Meteor.subscribe('thePlayers');
 
-  Template.addPlayerForm.events({
-    'submit form': function(event, template) {
-      event.preventDefault();
-      var playerNameVar = event.target.playerName.value;
-      var playerScoreVar = parseInt(event.target.playerScore.value);
-      var currentUserId = Meteor.userId();
-      PlayersList.insert({
-        name: playerNameVar,
-        score: playerScoreVar || 0,
-        createdBy: currentUserId
-      })
-      event.target.playerName.value = "";
-      event.target.playerScore.value = "";
+Template.leaderboard.helpers({
+  'player': function() {
+    return PlayersList.find({}, {sort: {score: -1, name: 1}});
+    // pass arbitrary {} so we can pass another argument e.g. sort
+    // Sort by + => DESC to ASC
+    // Sort by - => ASC to DESC
+  },
+  'listPlayerCount': function() {
+    var currentUserId = Meteor.userId();
+    return PlayersList.find({createdBy: currentUserId}).count();
+  },
+  'selectedClass': function() {
+    var playerId = this._id;
+    var selectedPlayer = Session.get('selectedPlayer');
+    if (playerId == selectedPlayer) {
+      return "selected";
     }
-  });
-  Template.removePlayerForm.events({
-    'click .remove': function() {
-      var selectedPlayer = Session.get('selectedPlayer');
-      if (confirm("Do you really want to remove " + PlayersList.findOne(selectedPlayer).name + "?")) { PlayersList.remove(selectedPlayer); }
-    }
-  });
-}
+  },
+  'showSelectedPlayer': function() {
+    var selectedPlayer = Session.get('selectedPlayer');
+    return PlayersList.findOne(selectedPlayer);
+  },
+  'averageScore': function() {
+    return PlayersList.find({}, {$avg: {score: 1}});
+  }
+});
+Template.leaderboard.events({
+  'click .player': function() {
+    var playerId = this._id;
+    Session.set('selectedPlayer', playerId);
+  },
+  'click .increment': function() {
+    var selectedPlayer = Session.get('selectedPlayer');
+    Meteor.call('modifyPlayerScore', selectedPlayer, 5);
+  },
+  'click .decrement': function() {
+    var selectedPlayer = Session.get('selectedPlayer');
+    Meteor.call('modifyPlayerScore', selectedPlayer, -5);
+  }
+});
 
-if (Meteor.isServer) {
-  Meteor.publish('thePlayers', function() {
-    var currentUserId = this.userId;
-    return PlayersList.find({createdBy: currentUserId});
-  });
-}
+Template.addPlayerForm.events({
+  'submit form': function(event, template) {
+    event.preventDefault();
+    var playerNameVar = event.target.playerName.value;
+    var playerScoreVar = parseInt(event.target.playerScore.value);
+    event.target.playerName.value = "";
+    event.target.playerScore.value = "";
+    Meteor.call('insertPlayerData', playerNameVar);
+  }
+});
+Template.removePlayerForm.events({
+  'click .remove': function() {
+    var selectedPlayer = Session.get('selectedPlayer');
+    if (confirm("Do you want to remove " + PlayersList.findOne(selectedPlayer).name + "?")) {
+      Meteor.call('removePlayerData', selectedPlayer);
+    }
+  }
+});
+
 
 
 // UserAccounts = new Mongo.Collection('users');
--- server/main.js
+++ server/main.js
@@ -5,3 +5,27 @@ Meteor.startup(() => {
   PlayersList = new Mongo.Collection('players');
   // We need to define this in the server as well or there will be an insert error on the client console
 });
+
+Meteor.publish('thePlayers', function() {
+  var currentUserId = this.userId;
+  return PlayersList.find({createdBy: currentUserId});
+});
+
+Meteor.methods({
+  'insertPlayerData': function(playerNameVar){
+    var currentUserId = Meteor.userId();
+    PlayersList.insert({
+        name: playerNameVar,
+        score: 0,
+        createdBy: currentUserId
+    });
+  },
+  'removePlayerData': function(selectedPlayer){
+    var currentUserId = Meteor.userId();
+    PlayersList.remove({_id: selectedPlayer, createdBy: currentUserId});
+  },
+  'modifyPlayerScore': function(selectedPlayer, scoreValue) {
+    var currentUserId = Meteor.userId();
+    PlayersList.update({_id: selectedPlayer, createdBy: currentUserId}, {$inc: {score: scoreValue}});
+  }
+});
